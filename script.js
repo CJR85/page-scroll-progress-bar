0a5a08b1b82cd1d7a59ca8938f944a336bc673bf
Calvin Roberts <roberts.calvin85@gmail.com>
2021-06-24 20:08:48 +0100

    Add function to cursorMeter, percent & progressBar.

---
+++ script.js
@@ -0,0 +1,15 @@
+let cursorMeter = document.getElementById('cursorMeter');
+document.addEventListener('mousemove', function (e) {
+  cursorMeter.style.top = e.clientY + 'px';
+  cursorMeter.style.left = e.clientX + 'px';
+});
+
+let percent = document.getElementById('percent');
+let progressBar = document.getElementById('progressBar');
+
+let totalHeight = document.body.scrollHeight - window.innerHeight;
+
+window.onscroll = function () {
+  let progress = (window.pageXOffset / totalHeight) * 100;
+  progressBar.style.width = progress + '%';
+};
